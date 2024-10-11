75f923183038f7eaf603a8277a4e6fb8e795ad14
jaehyeong <wogud1514@gmail.com>
2024-10-11 13:12:36 +0900

    init

---
+++ src/test/kotlin/jjfactory/command_query_generator/CommandQueryGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package jjfactory.command_query_generator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandQueryGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
